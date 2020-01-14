c7c42ad371294da23056084f93edfa8d201c72fe
唐林 <47147551@qq.com>
2020-01-14 14:26:29 +0800

    添加项目文件。

---
+++ SchoolHelper/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SchoolHelper.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
